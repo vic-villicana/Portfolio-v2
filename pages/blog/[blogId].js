dc62337faf59b3f01bf8ae284a110eca353907e5
vic-villicana <victor_u9@msn.com>
2023-03-21 17:37:38 -0700

    set upgetStaticProps and getStaticPaths for dynamic blog page

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -1,18 +1,20 @@
 import { useRouter } from "next/router"
 import {getEventById} from '../../dummy-data'
+import {getAllEvents} from '../../dummy-data'
 
 import BlogContent from '../../components/blog/blog-content'
 import BlogInfo from '../../components/blog/blog-info'
 import BlogHeading from '../../components/blog/blog-heading'
 
-export default function BlogPost(){
+export default function BlogPost(props){
 
     const router = useRouter()
     const blogId = router.query.blogId
 
-    const data = getEventById(blogId)
+    // const data = getEventById(blogId)
+    // console.log(data)
+    const data = props.data[0]
     console.log(data)
-
     if(!data){
         return <p>Loading bitch...</p>
     }
@@ -26,4 +28,29 @@ export default function BlogPost(){
             </BlogContent>
         </div>
     )
+}
+
+export async function getStaticProps(context){
+    const {params} = context
+    //this parameter will be used to searh for any relative data
+    //ex. as a paramter for a mongoose model query
+    const parameter = params.blogId
+    const data = getAllEvents()
+    const thePost = data.filter(theEvent => theEvent.id === parameter)
+    return {
+        props: {
+            data: thePost
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    const data = getAllEvents()
+
+    const thePaths = data.map(item => ({ params:{blogId: item.id} }))
+
+    return {
+        paths:thePaths,
+        fallback:true
+    }
 }
