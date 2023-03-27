09aae63af5afdbf659521a050c9043452ff9b89c
vic-villicana <victor_u9@msn.com>
2023-03-27 12:55:12 -0700

    remove unnecessary code

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,10 +3,7 @@ import ProjectSlide from '../../components/projects/project-slide'
 import ProjectSlider from '../../components/projects/project-slider'
 import ProjectHeader from '../../components/projects/project-header'
 import ContactForm from '../../components/home/contact-form'
-import {getCurrentProjects} from '../../dummy-data'
 
-import fs from 'fs/promises'
-import path from 'path'
 
 export default function Projects(props) {
     console.log(props)
@@ -24,18 +21,4 @@ export default function Projects(props) {
 
         </div>
     )
-}
-async function getData(){
-    const filePath = path.join(process.cwd(), "data", "dummy-data.json")
-    const readData = await fs.readFile(filePath)
-    const parsed = JSON.parse(readData)
-    return parsed
-}
-export async function getStaticProps() {
-    const projects = await getData()
-    return {
-        props:{
-            projects:projects.projects
-        }
-    }
 }
