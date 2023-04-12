a6d2b55a7be28fc77d6de23532f17268d5be67ae
vic-villicana <victor_u9@msn.com>
2023-04-12 09:17:37 -0700

    make ui updates to project header component

--- components/projects/project-header.js
+++ components/projects/project-header.js
@@ -1,25 +1,32 @@
-import classes from './project-header.module.css'
+import classes from './project-heading.module.css'
 import Link from 'next/link'
 export default function ProjectHeader(){
     return(
-        <div className={classes.PJheader}>
-            <div className={classes.headerImage}>
-                <img className={classes.picture} src="/images/design.jpg" alt="some picture of success"/>
-            </div>
-            <div className={classes.intro}>
+<div className={`${classes.headerTwo} ${classes.juiceHead}`}>
+        <div className={`${classes.void} ${classes.v2}`}>
+        <div className={classes.intro}>
                 <div className={classes.PJtext}>
-                    <h2>Building Your Future</h2>
+                    <h2>Current Projects</h2>
                     <p>
-                        Duis aute irure dolor in reprehenderit in 
-                        voluptate velit esse cillum dolore eu fugiat 
-                        nulla pariatur. Excepteur sint occaecat cupidatat 
-                        non proident.
+                        Here you will find some of my most
+                        favorite projects. Static websites, 
+                        Single Page Applications, and
+                        decentralized apps (or Dapps).
+                        These projects represent the core
+                        of my skills in AWS cloud architercture,
+                        mobile web app, and full-stack development.
+
                     </p>
                 </div>
                 <div className={classes.hollowBtn}>
                     <Link href="/contact">Lets Build!</Link>
                 </div>
             </div>
-        </div>
+        </div> 
+      </div>
+
+
+
+
     )
 }
