0ee9e82f3887b5f9b7757f9115b7be92471150dc
vic-villicana <victor_u9@msn.com>
2023-04-13 18:02:59 -0700

    set up dynamic page rendering

--- pages/projects/[projectId].js
+++ pages/projects/[projectId].js
@@ -2,15 +2,18 @@ import {useRouter} from 'next/router'
 
 import HelloForm from '../../components/contact/hello-form'
 import TipsyProject from '../../components/projects/tipsy-project'
+import JuicyProject from '../../components/projects/juicy-project'
 import NavBar from '../../components/layout/navbar'
 export default function ProjectPage() {
     const router = useRouter()
     const query =  router.query.projectId
+    console.log(query)
     return (
         <div>
-            <TipsyProject />
+            {query === 'v1' && <JuicyProject/>}
+            {query === 'v2' && <TipsyProject/>}
+            {query === 'v3' && "project coming soon!"}
             <HelloForm />
-
         </div>
     )
 }
